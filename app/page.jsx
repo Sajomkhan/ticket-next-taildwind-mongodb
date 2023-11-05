1a62d177965a6a4ffa5e294dcde559e0b67cea73
Saiful Ajom Khan <sajomkhan@gmail.com>
2023-11-06 00:00:10 +0600

    first commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,15 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [items, setItems] = useState([
+    { name: "Coffee", price: "6.55" },
+    { name: "Movie", price: "20.00" },
+    { name: "Candy", price: "4.55" },
+  ]);
+  const [total, setTotal] = useState(0);
+
   return (
     <main className="w-full min-h-screen flex flex-col items-center justify-center">
       <h1 className="text-2xl font-bold text-center mb-7">Expense Tracker</h1>
@@ -16,13 +25,20 @@ export default function Home() {
             type="text"
             placeholder="Enter $"
           />
-          <button className="bg-gray-900 p-2" type="submit">
+          <button className="bg-gray-900 hover:bg-gray-950 p-2" type="submit">
             +
           </button>
         </form>
         <ul>
-          <li>Apple</li>
-          <li>Orange</li>
+          {items.map((items, id) => (
+            <li key={id} className="my-3 w-full flex justify-between">
+              <div className="w-full flex justify-between mr-8">
+                <span>{items.name}</span>
+                <span>{items.price}</span>
+              </div>
+              <button className="bg-gray-900 hover:bg-gray-950 px-3">x</button>
+            </li>
+          ))}
         </ul>
       </div>
     </main>
