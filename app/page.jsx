abc02728cb67b0a08d4234eaf81239dd8c29e9c2
Saiful Ajom Khan <sajomkhan@gmail.com>
2023-11-05 23:22:40 +0600

    Add a branch

--- app/page.jsx
+++ app/page.jsx
@@ -4,10 +4,27 @@ export default function Home() {
   return (
     <main className="w-full min-h-screen flex flex-col items-center justify-center">
       <h1 className="text-2xl font-bold text-center mb-7">Expense Tracker</h1>
-      <ul>
-        <li>Apple</li>
-        <li>Orange</li>
-      </ul>
+      <div className="bg-slate-800 p-4 rounded-md text-[var(--textLight)]">
+        <form className="grid grid-cols-6 gap-4 mb-5">
+          <input
+            className="col-span-3 p-2 border"
+            type="text"
+            placeholder="Enter Item"
+          />
+          <input
+            className="col-span-2 p-2 border"
+            type="text"
+            placeholder="Enter $"
+          />
+          <button className="bg-gray-900 p-2" type="submit">
+            +
+          </button>
+        </form>
+        <ul>
+          <li>Apple</li>
+          <li>Orange</li>
+        </ul>
+      </div>
     </main>
   );
 }
