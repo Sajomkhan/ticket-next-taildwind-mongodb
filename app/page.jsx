d6d00047636787380857a31918a9dc9217a3202b
Saiful Ajom Khan <sajomkhan@gmail.com>
2023-11-06 14:58:34 +0600

    display total

--- app/page.jsx
+++ app/page.jsx
@@ -13,8 +13,8 @@ export default function Home() {
   return (
     <main className="w-full min-h-screen flex flex-col items-center justify-center">
       <h1 className="text-2xl font-bold text-center mb-7">Expense Tracker</h1>
-      <div className="bg-slate-800 p-4 rounded-md text-[var(--textLight)]">
-        <form className="grid grid-cols-6 gap-4 mb-5">
+      <div className="bg-slate-800 p-8 rounded-md text-[var(--textLight)]">
+        <form className="grid grid-cols-6 gap-4 mb-12">
           <input
             className="col-span-3 p-2 border"
             type="text"
@@ -23,7 +23,7 @@ export default function Home() {
           <input
             className="col-span-2 p-2 border"
             type="text"
-            placeholder="Enter $"
+            placeholder="$"
           />
           <button className="bg-gray-900 hover:bg-gray-950 p-2" type="submit">
             +
@@ -32,14 +32,22 @@ export default function Home() {
         <ul>
           {items.map((items, id) => (
             <li key={id} className="my-3 w-full flex justify-between">
-              <div className="w-full flex justify-between mr-8">
+              <div className="w-full flex justify-between bg-gray-900 p-3 mr-3">
                 <span>{items.name}</span>
                 <span>{items.price}</span>
               </div>
-              <button className="bg-gray-900 hover:bg-gray-950 px-3">x</button>
+              <button className="bg-gray-900 hover:bg-gray-950 px-6">x</button>
             </li>
           ))}
         </ul>
+        {items.length < 1 ? (
+          ""
+        ) : (
+          <div className="font-semibold grid grid-cols-6 mt-11 p-3">
+            <span className="col-span-5">Total</span>
+            <span>$ {total}</span>
+          </div>
+        )}
       </div>
     </main>
   );
